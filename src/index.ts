7b8b0a6831e3478a11ff626ee7f498e28b3c876e
vibovenkat123 <vaibhav@vaibhavvenkat.com>
2023-07-05 22:16:17 -0500

    fix: export git types

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,5 @@ export {
   ShellCommandError,
 } from "./shell/types";
 
-export {GitRepo} from "./git";
-export {
-    GitOpts,
-    GitError,
-    GitSafeResponse
-} from "./git/types";
+export { GitRepo } from "./git";
+export { GitOpts, GitError, GitSafeResponse } from "./git/types";
